aea955f04a505652fedb8bf19b3b04bc28551a5a
Peter Staab <peter.staab@gmail.com>
2016-11-09 12:06:01 -0500

    updated TropicalMath.jl

--- sci-comp-notes/html/TropicalMath.jl
+++ sci-comp-notes/html/TropicalMath.jl
@@ -1,8 +1,8 @@
 module TropicalMath
 
-export ⊕,⊗
+export ⊕,⊗,trop_pow
 
-function ⊕{T<:Number}(x::T,y::T)
+function ⊕{T1<:Number,T2<:Number}(x::T1,y::T2)
     min(x,y) 
 end
 
@@ -10,16 +10,16 @@ function ⊕{T<:Number}(x::T...)
     minimum(x)
 end
 
-function ⊕{T<:Number}(A::Array{T,1},B::Array{T,1})
+function ⊕{T1<:Number,T2<:Number}(A::Array{T1,1},B::Array{T2,1})
     length(A)==length(B) || throw(ArgumentError("The lengths of the vectors do not match"))
     map(⊕,A,B)
 end
 
-function ⊕{T<:Number}(A::Array{T,2},B::Array{T,2})
+function ⊕{T1<:Number,T2<:Number}(A::Array{T1,2},B::Array{T2,2})
     map(⊕,A,B)
 end
 
-function ⊗{T<:Number}(x::T,y::T)
+function ⊗{T1<:Number,T2<:Number}(x::T1,y::T2)
     x+y
 end
 
@@ -27,25 +27,35 @@ function ⊗(x::Number...)
     sum(x)
 end
 
-function ⊗{T<:Number}(A::Array{T,1},B::Array{T,1})
+function ⊗{T1<:Number,T2<:Number}(A::Array{T1,1},B::Array{T2,1})
     length(A)==length(B) || throw(ArgumentError("The lengths of the vectors do not match"))
-    local v=[]
+    local v=zeros(Float64,length(A))
+    #println(v)
     for i=1:length(A)
-        push!(v,A[i]⊗B[i])
-        
+        v[i]=A[i]⊗B[i]
     end
     ⊕(v...)
 end
 
-function ⊗{T<:Number}(A::Array{T,2},B::Array{T,2})
+function ⊗{T1<:Number,T2<:Number}(A::Array{T1,2},B::Array{T2,2})
     size(A,2)==size(B,1) || throw(ArgumentError("The matrices must be compatible."))
-    local C = zeros(Int,size(A,1),size(B,2))
+    local C = zeros(Float64,size(A,1),size(B,2))
     for i=1:size(A,1) 
         for j=1:size(B,2)
             C[i,j]=A[i,:]⊗B[:,j]
         end
     end
     C
+end
+
+function trop_pow{T<:Number}(A::Array{T,2},n::Integer)
+    size(A,1)==size(A,2) || throw(ArgumentError("The matrix must be square to raise it to a power."))
+    n>0 || throw(ArgumentError("you can only raise it to a positive power"))
+    B=copy(A)
+    for i=2:n
+       B=B⊗A
+    end
+    B
 end
 
 end
